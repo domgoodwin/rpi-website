a4ff8b8bbcf16c1cfd6aaeaa0feb8d65c3ddd632
domgoodwin <d0m182goodwin@gmail.com>
2018-01-06 16:30:34 +0000

    Updated with ping and live display functionality

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var os = require('os');
+
 var express = require('express')
   , logger = require('morgan')
   , app = express()
   , template = require('jade')
+  , sse = require('./sse')
+
+var http = require('http').Server(app)
+var io = require('socket.io')(http);
+
+var ping = require('ping')
+//var http = require('http').Server(app);
+
+
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+app.set('view engine', 'jade');
+
+
 
 var fil = '/source/templates/';
 
@@ -17,6 +30,10 @@ app.get('/', function (req, res, next) {
     } catch (e) {
         next(e)
     }
+})
+
+app.get('*.js', function(req, res, next){
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/functions', function (req, res, next) {
@@ -60,26 +77,60 @@ app.get('/api', function (req, res, next) {
 })
 
 app.get('/dashboard', function (req, res, next) {
+    //res.sseSetup()
     var pageName = "dashboard.jade";
     var memUsageNum = formatUsageString((os.freemem()/os.totalmem()));
     var memUse = os.freemem() + ' / ' +  os.totalmem() + ' : ' + memUsageNum + '%';
     var cpuUsageArr = os.loadavg();
     var cUse = '1:' + formatUsageString(cpuUsageArr[0]) + '% / 5:' + formatUsageString(cpuUsageArr[1]) + '% / 15:' + formatUsageString(cpuUsageArr[2]) + '%';
     console.log(cUse);
-    //console.log( 'CPU Usage (%): ' + v );
     try {
+
         var html = template.compileFile(__dirname + fil + pageName)({ title: 'Dashboard', cpuUsage: cUse, memoryUsage: memUse})
-        res.send(html)
+        res.send(html);
+        //pingDevices();
     } catch (e) {
         next(e)
+        console.log(e)
     }
-})
+
+});
+
+io.on('connection', function(socket){
+  console.log("Connected")
+  socket.on('ping-get', function(){
+    pingDevices()
+  });
+});
 
 function formatUsageString(numberToProcess){
   return (numberToProcess * 100).toFixed(2);
 }
 
+var pingDevices = function(callback){
+  var devices = [
+    {host: 'DOM-NAS', ip: '192.168.0.100'},
+    {host: 'DOM-PC', ip: '192.168.0.14'},
+    {host: 'DOM-MOBILE', ip: '192.168.0.200'},
+    {host: 'barbara-iPhone', ip: '192.168.0.201'},
+    {host: 'E-TBS-IPHONE', ip: '192.168.0.202'}
+  ]
+  var count = 0
+  devices.forEach(function(device){
 
-app.listen(process.env.PORT || 3000, function () {
+    var host = device.host
+
+    ping.sys.probe(device.ip, function(isAlive){
+          count++
+        var msg = isAlive ? host + ' is alive' : host + ' is dead';
+        console.log("sending :" + msg)
+        io.emit('ping', msg);
+      });
+
+  });
+}
+
+
+http.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
